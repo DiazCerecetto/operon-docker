c14149f82446880b15bccf5fb09d5f3f125d4696
DiazCerecetto <diaz.cerecetto@gmail.com>
2024-07-28 21:01:44 -0300

    fix r2

--- code/SRutils.py
+++ code/SRutils.py
@@ -117,6 +117,7 @@ def entrenar_evaluar_modelo(iteraciones, path_train, path_test, path_resultados,
             y = df.iloc[:, -1].values
 
             if np.isnan(X).any() or np.isnan(y).any():
+                print("Valores NaN en el archivo ", nombre_csv)
                 continue
             y_pred = predicciones[j].predict(X)
             
@@ -127,14 +128,19 @@ def entrenar_evaluar_modelo(iteraciones, path_train, path_test, path_resultados,
                 print()
                 df_salida.loc[j] = [obtener_funcion(f), 0, predicciones[j].get_model_string(), np.nan]
                 continue
-
-            r2 = predicciones[j].score(X, y)
+        
+            r2 = 0
+            try:
+                r2 = predicciones[j].score(X, y)
+            except:
+                r2 = np.nan
+    
             rmse = calcular_rmse(y, y_pred)
             df_salida.loc[j] = [obtener_funcion(f), r2, simplify_expression(predicciones[j].get_model_string()), rmse]
 
-        
-        df_salida.to_csv(resultados, index=False)
-    tiempo_total = time.time() - tiempo_inicio
+            
+            df_salida.to_csv(resultados, index=False)
+            tiempo_total = time.time() - tiempo_inicio
     return tiempo_total, tiempos_iteraciones
 
 
