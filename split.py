d56913d7101fbea67e00079087ae94a83b585f8f
DiazCerecetto <diaz.cerecetto@gmail.com>
2024-09-10 16:50:03 -0300

    all results

--- split.py
+++
@@ -1,8 +0,0 @@
-#given a list of csv files, split them in half and save them on Ferreira/train/ and Ferreira/test/
-# Usage: python split.py 
-import os
-local_path = os.getcwd()
-files = ['ferreira9.csv','ferreira10.csv',
-            'ferreira11.csv','ferreira12.csv',
-            'ferreira13.csv','ferreira14.csv',
-            'ferreira15.csv','ferreira16.csv']
